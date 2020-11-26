52cfbc54591cd330254c29bb8ec3527fb16ee700
chzcb <chzcb.04@163.com>
2020-11-26 23:04:38 +0800

    hide data

--- dhp-rpc-core/src/main/java/org/dhp/core/rpc/Message.java
+++ dhp-rpc-core/src/main/java/org/dhp/core/rpc/Message.java
@@ -1,11 +1,13 @@
 package org.dhp.core.rpc;
 
 import lombok.Data;
+import lombok.ToString;
 
 /**
  * @author zhangcb
  */
 @Data
+@ToString(exclude = "data")
 public class Message {
     public static final int HEAD_LEN = 9;
     public static final int MAX_PACKET_LEN = 32 * 1024 * 1024;
